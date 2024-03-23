edf8d35d3123099fcc31c594793ab5f0471fff67
BradTheeStallion <156148735+BradTheeStallion@users.noreply.github.com>
2024-03-23 08:13:33 -0230

    Add files via upload
    I started working on this program in VSCode, now it's time to commit.

---
+++ CoolStuffQAP4.py
@@ -0,0 +1,326 @@
+#Nifty Functions
+#By Brad Ayers
+#February 2024 - Present
+
+def DataFile(Filename):
+    #DataFile needs to be copy-pasted and tailored to the program. The function is designed to save all variables in the global namespace to a file, with the exception of modules and functions. The function also includes a loading bar.
+    #DataFile("filename.dat")
+    import types
+    import time
+    import random
+    PHRASE_1 = "Saving: Please Stand By"
+    PHRASE_2 = "Data Saved Successfully."
+    DataList = []
+    Frames1 = []
+    Frames2 = []
+    Frames3 = []
+    def FrameLoop(Phrase, FrameList):
+        #Just avoiding repetition within the outer function.
+        Counter1 = 0
+        Counter2 = 1
+        while Counter1 <= len(Phrase):
+            FrameList.append(Phrase[0:Counter2])
+            Counter1 += 1
+            Counter2 += 1
+    AllVars = dict(globals())
+    for Name, Var in AllVars.items():
+        if type(Var) not in [types.ModuleType, types.FunctionType] and not Name.startswith("_"):
+            DataList.append(Var)
+    f = open(Filename, "a")
+    for item in DataList:
+        if item != DataList[-1]:
+            f.write("{},".format(str(item)))
+        else:
+            f.write("{}\n".format(str(item)))
+    f.close()
+    FrameLoop(PHRASE_1, Frames1)
+    for i in range(0,3):
+        for j in range(0,4):
+            Frames1.append(PHRASE_1 + (j * " ."))    
+    for i in range(0,41):
+        Frames2.append(i * "|")
+    FrameLoop(PHRASE_2, Frames3)
+    for frame in Frames1:
+        print("\r" + frame, end="")
+        time.sleep(0.03)
+    print("\r" + " " * len(Frames1[-1]), end="")
+    LoadingDelay = random.randint(20,34)
+    for frame in Frames2:
+        if frame == Frames2[LoadingDelay]:
+            print("\r" + frame, end="")
+            time.sleep(random.uniform(0.6, 0.9))
+        else:
+            print("\r" + frame, end="")
+            time.sleep(0.05)
+    print("\r" + " " * len(Frames2[-1]), end="")
+    for frame in Frames3:
+        if frame != Frames3[-1]:
+            print("\r" + frame, end="")
+            time.sleep(0.03)
+        else:
+            print("\r" + frame, end="")
+            time.sleep(0.5)
+    print("\r" + " " * len(Frames3[-1]), end="")
+    
+def Hst(PriceInput):
+    #This little function takes an integer or float as input and calculates the HST, the total with HST, formats both of those values (and the input value) to be receipt-ready, and returns all 5 values. This is designed to be used on value that has already been validated at an earlier step in the code.
+    HST_RATE = 0.15
+    HstAmt = PriceInput * HST_RATE
+    TotalAmt = PriceInput + HstAmt
+    PriceInputDSP = "${:,.2f}".format(PriceInput)
+    HstAmtDsp = "${:,.2f}".format(HstAmt)
+    TotalAmtDsp = "${:,.2f}".format(TotalAmt)
+    #The values are returned as a tuple, an object that can be indexed like a list.
+    return HstAmt, TotalAmt, PriceInputDSP, HstAmtDsp, TotalAmtDsp
+
+def AnyKey(Prompt):
+    #The following function advances the program after any key is pressed. First, end='' and flush=True are used to make the print function behave (keeping the cursor at the end of that line) Next, the code checks the operating system and then proceeds accordingly. In windows, the msvcrt.getch() function records the keystroke as a byte string, but doesn't produce anything in the console. In Unix systems (such as Linux or MacOS), the terminal is set to raw mode in the try statement,temporarilly (for one keystroke) preventing the character from appearing on the console. The finally statement brings the terminal back out of raw mode. This isn't the extra feature lol. I chose to import the modules within the function because it crashed the code for me otherwise.
+    import sys
+    print(Prompt, end='', flush=True)
+    if sys.platform.startswith('win'):
+        import msvcrt
+        msvcrt.getch()
+    else:
+        import tty
+        import termios
+        FileDesc = sys.stdin.fileno()
+        OldSettings = termios.tcgetattr(FileDesc)
+        try:
+            tty.setraw(FileDesc)
+            sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(FileDesc, termios.TCSADRAIN, OldSettings)
+            
+def Whoops(Prompt):
+    #This function is a little escape hatch to allow the users to get out of a program without running it. Example use:
+    # Main program
+    #while True:
+        #if not Whoops("If you accidentally chose Option 1,\ntype END and press return to go back to the main menu.\nOtherwise, press return to continue: "):
+            #break
+    print(Prompt, end='', flush=True)
+    Oops = input().upper()
+    print()
+    if Oops == "END":
+        return False
+    else:
+        return True
+
+def Padding(string):
+    #Adds blank lines before and after print statement
+    print()
+    print(string)
+    print()
+
+def NotBlank(Prompt):
+    #Just a little function to make sure a field isn't blank. This is incorporated in several functions in this library.
+    while True:
+        UserInput = input(Prompt)
+        if not UserInput:
+            Padding("Error: Field cannot be blank.")
+            continue
+        else:
+            return UserInput
+        
+def ValidNameAdd(Prompt):
+    #Makes sure field isn't blank and formats cities to my preference
+    import string
+    while True:
+        UserInput = NotBlank(Prompt)
+        if not UserInput:
+            continue
+        else:
+            return string.capwords(UserInput) + ",", UserInput.title()
+            
+            
+def MoneyFloat(Prompt):
+    #This function uses regex to ensure that a number is not only a float, but has no more than the two decimal places expected for a monetary value. There were fewer monetary inputs than expected, but we're proud of this and it will definitely be recycled in future code.
+    import re
+    pattern = r'^\d+(\.\d{1,2})?$'
+    while True:
+        UserFloat = NotBlank(Prompt)
+        if not UserFloat:
+            continue
+        elif not re.match(pattern, UserFloat):
+            Padding("Error: Please double check your value.")
+        else:
+            return float(UserFloat), "${:,.2f}".format(float(UserFloat))
+        
+def ValidPhone(Prompt):
+    #This functions validates phone numbers to suit my preferred input format.
+    while True:
+        PhoneNum = NotBlank(Prompt)
+        if not PhoneNum:
+            continue
+        elif len(PhoneNum) != 10:
+            Padding("Error: Phone number must be 10 digits.")
+        elif not PhoneNum.isdigit():
+            Padding("Error: Phone number must be digits only.")
+        else:
+            return PhoneNum, f"({PhoneNum[0:3]}) {PhoneNum[3:6]}-{PhoneNum[6:]}"
+
+def ValidPost(Prompt):
+    #This functions validates postal codes to suit my preferred input format.
+    while True:
+        PostCode = NotBlank(Prompt)
+        if not PostCode:
+            continue
+        elif len(PostCode) != 6 or not PostCode[0].isalpha() or not PostCode[2].isalpha() or not PostCode[4].isalpha() or not PostCode[1].isdigit() or not PostCode[3].isdigit() or not PostCode[5].isdigit():
+            Padding("Error: Invalid postal code.")
+        else:
+            return PostCode.upper(), f"{PostCode[0:3]} {PostCode[3:]}".upper()
+    
+def ValidProv(Prompt):
+    #This functions validates province abbreviations to suit my preferred input format.
+    while True:
+        ProvList = ["NL", "PE", "NS", "NB", "QC", "ON", "MB", "SK", "AB", "BC", "YT", "NT", "NU"]
+        Province = NotBlank(Prompt).upper()
+        if len(Province) != 2:
+            Padding("Error: Province must be two characters (XX).")
+        elif not Province.isalpha():
+            Padding("Error: Province must be two letters (XX).")
+        elif Province == "PQ":
+            Province = "QC"
+            return Province
+        elif Province == "NF":
+            Province = "NL"
+            return Province
+        elif Province not in ProvList:
+            Padding(f"Error: Value not recognized as Canadian Province.\n{ProvList}")
+        else:
+            return Province
+
+def ValidPlate(Prompt):
+    #This functions validates licence plates to suit my preferred input format.
+    while True:
+        PlateNum = NotBlank(Prompt)
+        if len(PlateNum) != 6:
+            Padding("Error: Plate number must be six characters.")
+        elif not PlateNum[0:3].isalpha() or not PlateNum[3:].isdigit():
+            Padding("Error: Plate number must be three letters followed by three numbers.")
+        else:
+            return PlateNum.upper(), f"{PlateNum[0:3]} {PlateNum[3:]}".upper()
+        
+def ValidInt(Prompt):
+    #Use example:
+    #MyNum = ValidInt("Number?: ")
+    #print(MyNum)
+    while True:
+        UserInput = NotBlank(Prompt)
+        try:
+            return int(UserInput) 
+        except:
+            Padding("Error: Value must be a whole number.")
+            continue
+
+def ValidFloat(Prompt):
+    #Use example:
+    #MyFloat = ValidFloat("Number?: ")
+    #print(MyFloat)
+    while True:
+        UserInput = NotBlank(Prompt)
+        try:
+            float(UserInput)
+            return UserInput, "${:,.2f}".format(UserInput)
+        except:
+            Padding("Error: Value must be a number.")
+            continue
+            
+def ValidYN(Prompt, Choice1 = 'Y', Choice2 = 'N', *args):
+    #Makes sure user only enters Y or N (or specifications if different is required)
+    while True:
+        UserInput = NotBlank(Prompt).upper()
+        if UserInput != Choice1 and UserInput != Choice2 and UserInput not in args:
+            if not args:
+                Padding(f"Error: Value must be {Choice1} or {Choice2}.")
+                continue
+            else:
+                Padding(f"Error: Value must be {Choice1} or {Choice2} or {' '.join(map(str, args))}.")
+                continue
+        else:
+            return UserInput
+            
+def ValiDate(Prompt):
+    #Returns a date as a string and an object in my preferred format
+    from datetime import datetime
+    AnyKey(Prompt)
+    while True:
+        Year = NotBlank("\nPlease enter the year (####): ")
+        if not Year.isdigit() or len(Year) != 4:
+            Padding("Error: Year must be four digits (####).")
+            continue
+        else:
+            break
+    while True:
+        Month = ValidInt("Please enter the month (1-12): ")
+        if not (1 <= Month <= 12):
+            Padding("Error: Month needs to be a number from 1 to 12.")
+            continue
+        else:
+            if len(str(Month)) == 1:
+                Month = f"0{Month}"
+            else:
+                Month = str(Month)
+        break
+    while True:
+        Day = ValidInt("Please enter the day (1-31): ")
+        try:
+            Date = datetime.strptime(f"{Year}-{Month}-{Day}", "%Y-%m-%d").date()
+            if len(str(Day)) == 1:
+                Day = f"0{Day}"
+            else:
+                Day = str(Day)
+            return Date, f"{Year}-{Month}-{Day}"
+        except:
+            Padding("Error: Invalid date.")
+            continue
+
+def IntMoreZero(Prompt):
+    #Checks to see if an integer is > 0
+    while True:
+        Num = ValidInt(Prompt)
+        if Num <= 0:
+            Padding("Error: Number cannot be 0 or less.")
+            continue
+        else:
+            return Num
+
+def MFMoreZero(Prompt):
+    #Checks to see if dollar value is > 0
+    while True:
+        Num = MoneyFloat(Prompt)[0]
+        if Num <= 0:
+            Padding("Error: Number cannot be 0 or less.")
+            continue
+        else:
+            return Num
+        
+def FrstNxtMnth():
+    import datetime
+    if datetime.datetime.now().month < 12:
+        next_month_year = datetime.datetime.now().year
+    else:
+        next_month_year = datetime.datetime.now().year + 1
+    if datetime.datetime.now().month < 12:
+        next_month = datetime.datetime.now().month + 1
+    else:
+        next_month = 1
+    return datetime.datetime(next_month_year, next_month, 1), datetime.datetime(next_month_year, next_month, 1).strftime("%Y-%m-%d")
+
+def UpdateFrstLn(Filename, NewValue):
+    f = open(Filename, 'r')
+    lines = f.readlines()
+    lines[0] = NewValue + '\n'
+    f = open(Filename, 'w')
+    f.writelines(lines)
+    f.close()
+
+def ValidEmail(Prompt):
+    import re
+    EMAIL_REGEX = re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b")
+    while True:
+        Email = NotBlank(Prompt)
+        if not EMAIL_REGEX.match(Email):
+            Padding("Error: Invalid format for an email address.")
+            continue
+        else:
+            return Email
---
+++ QAP4.py
@@ -0,0 +1,259 @@
+#QAP4:  A  program  to  enter  and  calculate  new  insurance  policy information for the One  Stop  Insurance  Company's customers.
+#By Brad Ayers
+#March 14 - 22, 2024
+
+#Imports
+import datetime
+import CoolStuffQAP4 as QAP4 #Most functions used in this program are here
+
+#Functions
+#A version of this function exists in CoolStuff, but in order to work it needs to be copy-pasted and tailored to the program.
+def DataFile(Filename):
+    #This function automates the data file saving process, including a loading bar. Specify the name of the file as an arugment when calling the function.
+    #DataFile("filename.dat")
+    import types
+    import time
+    import random
+    PHRASE_1 = "Saving: Please Stand By"
+    PHRASE_2 = "Data Saved Successfully."
+    DataList = []
+    Frames1 = []
+    Frames2 = []
+    Frames3 = []
+    def FrameLoop(Phrase, FrameList):
+        #Just avoiding repetition within the outer function.
+        Counter1 = 0
+        Counter2 = 1
+        while Counter1 <= len(Phrase):
+            FrameList.append(Phrase[0:Counter2])
+            Counter1 += 1
+            Counter2 += 1
+    AllVars = dict(globals())
+    for Name, Var in AllVars.items():
+        if type(Var) not in [types.ModuleType, types.FunctionType] and not Name.startswith("_"):
+            DataList.append(Var)
+    f = open(Filename, "a")
+    f.write("Date:       Policy #:  Name:              Address:                Tel #:          Email:                 # Cars:  Premium:\n")
+    f.write("--------------------------------------------------------------------------------------------------------------------------\n")
+    f.write(f"{DataList[0]:<10s}  {DataList[3]:<4d}       {DataList[10][0]:<1s}. {DataList[11]:<15s} {DataList[12]:<20s}    {DataList[16]:<14s}  {DataList[17]:<25s} {DataList[18]:<2d}  {QAP4.Hst(DataList[22])[2]:>10s}\n")
+    f.write(f"                                          {DataList[13]} {DataList[14]}\n")
+    f.write(f"                                          {DataList[15]}\n")
+    f.close()
+    FrameLoop(PHRASE_1, Frames1)
+    for i in range(0,3):
+        for j in range(0,4):
+            Frames1.append(PHRASE_1 + (j * " ."))    
+    for i in range(0,41):
+        Frames2.append(i * "|")
+    FrameLoop(PHRASE_2, Frames3)
+    for frame in Frames1:
+        print("\r" + frame, end="")
+        time.sleep(0.03)
+    print("\r" + " " * len(Frames1[-1]), end="")
+    LoadingDelay = random.randint(20,34)
+    for frame in Frames2:
+        if frame == Frames2[LoadingDelay]:
+            print("\r" + frame, end="")
+            time.sleep(random.uniform(0.6, 0.9))
+        else:
+            print("\r" + frame, end="")
+            time.sleep(0.05)
+    print("\r" + " " * len(Frames2[-1]), end="")
+    for frame in Frames3:
+        if frame != Frames3[-1]:
+            print("\r" + frame, end="")
+            time.sleep(0.03)
+        else:
+            print("\r" + frame, end="")
+            time.sleep(0.5)
+    print("\r" + " " * len(Frames3[-1]), end="")
+
+while True:
+
+    #Constants
+    TODAY = datetime.datetime.now().strftime("%Y-%m-%d")
+    MONTHS = 8 #Number of months in the payment plan
+
+    #The 7 constants below are saved in the file Defaults.dat
+    #I didn't include HST since it's baked into my Hst() function
+
+    #f = open("Defaults.dat", "r")
+    f = open("SecondHalf/Python/QAP4/Defaults.dat", "r")
+    NEXT_POL_NUM = int(f.readline()) #Next  policy  number
+    BAS_PREM = float(f.readline()) #Basic  premium
+    DISC_ADD_CAR = float(f.readline()) #Discount  for additional  cars
+    LIABILITY = float(f.readline()) #Cost  of  extra  liability  coverage
+    GLASS = float(f.readline()) #Cost  of  glass  coverage
+    LOANER = float(f.readline()) #Cost  for  loaner  car coverage
+    MONTHLY_PROC_FEE = float(f.readline()) #Processing fee for monthly payments
+    f.close()
+
+    #Inputs 1
+    #I chose to divide the inputs and processing into two sections so I could validate the downpayment against the total.
+    print()
+    if not QAP4.Whoops("Welcome to One Stop Insurance's Policy Detail Form. Press any key to continue.\nIf you are here by mistake, type END: "):
+        print("Thank you for using the QAP4 claim processing program!")
+        print()
+        break
+    CustFrstName = QAP4.ValidNameAdd("Please enter the customer's first name: ")[1]
+    print()
+    CustLstName = QAP4.ValidNameAdd("Please enter the customer's last name: ")[1]
+    print()
+    StAdd = QAP4.ValidNameAdd("Please enter the customer's street address: ")[1]
+    print()
+    City = QAP4.ValidNameAdd("Please enter the customer's city of residence: ")[0]
+    print()
+    Prov = QAP4.ValidProv("Please enter the customer's province of residence (XX): ")
+    print()
+    PstCode = QAP4.ValidPost("Please enter the customer's postal code (X#X#X#): ")[1]
+    print()
+    PhoNum = QAP4.ValidPhone("Please enter the customer's phone number (##########): ")[1]
+    print()
+    Email = QAP4.ValidEmail("Please enter the customer's email address: ")
+    print()
+    NumCars = QAP4.IntMoreZero("Please enter the number of cars to be insured: ")
+    print()
+    ExtLiab = QAP4.ValidYN("Will the customer opt for extra liability up to $1,000,000? (Y or N): ")  
+    print()
+    Glass = QAP4.ValidYN("Will the customer opt for glass coverage? (Y or N): ")
+    print()
+    Loaner = QAP4.ValidYN("Will the customer opt for a loaner car? (Y or N): ")
+    print()
+    
+    #Processing 1
+    Premium = BAS_PREM
+    for i in range(NumCars):
+        if i != 0:
+            Premium += (BAS_PREM * DISC_ADD_CAR)
+
+    TotExCost = 0
+    if ExtLiab == "Y":
+        TotExCost += (NumCars * LIABILITY)
+    if Glass == "Y":
+        TotExCost += (NumCars * GLASS)
+    if Loaner == "Y":
+        TotExCost += (NumCars * LOANER)
+    
+    TotPrem = QAP4.Hst(Premium + TotExCost)
+    
+    #Inputs 2
+    PayType = QAP4.ValidYN("Will the customer opt for full or monthly payment? (F or M)?: ", "F", "M")
+    if PayType == 'M':
+        DPStatus = QAP4.ValidYN("Will the customer opt to make a down payment? (Y or N): ")
+        if DPStatus == 'Y':
+            while True:
+                DownPay = QAP4.MFMoreZero("Please enter the amount of the downpayment: ")
+                if DownPay > TotPrem[1]:
+                    QAP4.Padding("Error: Downpayment cannot be more than total.")
+                    continue
+                else:
+                    break
+        else:
+            DownPay = 0
+    else:
+        DownPay = 0
+    print()
+        
+    ClaimList = []
+    while True:
+        if not QAP4.Whoops("Press return to enter the customer's previous claims.\nIf there are no previous claims, type END: "):
+            break
+        Listy = []
+        Listy.append(QAP4.ValidInt("Please enter previous claim number: "))
+        Listy.append(QAP4.ValiDate("Press return to enter previous claim date: ")[1])
+        Listy.append(QAP4.MoneyFloat("Please enter previous claim amount: ")[1])
+        ClaimList.append(Listy)
+        print()
+        
+    #Processing 2
+    AmtDue = (TotPrem[1] - DownPay)
+    MthlyPay = ((TotPrem[1] + MONTHLY_PROC_FEE - DownPay)/MONTHS)
+         
+    #Output
+    print()
+    print("          One Stop Insurance Company")
+    print("           Insurance Claim Invoice:")
+    print("______________________________________________")
+    print()
+    print(f"  Invoice Date: {TODAY:<10s}   Policy No: {NEXT_POL_NUM:>4d}")
+    print()
+    print(f"  Customer:")
+    print()
+    print(f"    {CustFrstName:<{len(CustFrstName)}s} {CustLstName:<{27 - len(CustFrstName)}s}")
+    print(f"    {StAdd:<29s}")
+    print(f"    {City:<{len(City)}s} {Prov:<2s} {PstCode:<7s}")
+    print(f"    {PhoNum:<14s}")
+    print(f"    {Email:<20s}")
+    print()
+    print("----------------------------------------------")
+    print()
+    print(f"    Number of Cars: {NumCars:<2d}")
+    print()
+    print(f"       Premium:              {QAP4.Hst(Premium)[2]:>10s}")
+    if ExtLiab == "Y":
+        print(f"       Extra Liability:         {QAP4.Hst(NumCars * LIABILITY)[2]:>7s}")
+    if Glass == "Y":
+        print(f"       Glass Coverage:          {QAP4.Hst(NumCars * GLASS)[2]:>7s}")
+    if Loaner == "Y":    
+        print(f"       Loaner Car:              {QAP4.Hst(NumCars * LOANER)[2]:>7s}")
+    print(f"       Total Extras:         {QAP4.Hst(TotExCost)[2]:>10s} ")
+    print(f"       Subtotal:             {TotPrem[2]:>10s}")
+    print(f"       HST:                  {TotPrem[3]:>10s}")
+    if PayType == 'M':
+        print("                             ----------")
+        print(f"       Total:                {TotPrem[4]:>10s}")
+        print(f"       Downpayment:          {QAP4.Hst(DownPay)[2]:>10s}")
+        print("                             ----------")
+        print(f"       Total Due:            {QAP4.Hst(AmtDue)[2]:>10s}")
+        print()
+        print("----------------------------------------------")
+        print()
+        print(f"  Processing Fee:    {QAP4.Hst(MONTHLY_PROC_FEE)[2]:<6s}  (one time only)")
+        print(f"  Monthly Payment:   {QAP4.Hst(MthlyPay)[2]:<8s} (for {MONTHS} months)")
+        print()
+        print(f"  First Payment Due: {QAP4.FrstNxtMnth()[1]:<10s}")
+        print()
+    else:
+        print("                             ----------")
+        print(f"       Total Due:            {TotPrem[4]:>10s}")
+        print()
+    print("----------------------------------------------")
+    print()
+    print("               Previous Claims")
+    print("               ---------------")
+    print("   Claim #          Date            Amount")
+    print("  -----------------------------------------")
+
+    if ClaimList:
+        for item in ClaimList:
+            print(f"   {item[0]:<6d}        {item[1]:^10s}    {item[2]:>11s}")
+    else:
+        print("              No Previous Claims.")
+
+    print("  -----------------------------------------")
+    print()
+    print("          One Stop: Look No Further!")
+    print()
+    print("----------------------------------------------")
+    print()
+    
+    DataFile("SecondHalf/Python/QAP4/Claims.dat")
+    f = open("SecondHalf/Python/QAP4/Claims.dat", "a")
+    if ClaimList:
+        for item in ClaimList:
+            f.write("{}\n".format(f"Previous Claim #: {item[0]}, Date: {item[1]}, Amt: {item[2]}"))
+            f.write("\n")
+    else:
+        f.write("No Previous Claims.\n")
+        f.write("\n")
+        f.write("\n")
+    f.close()
+    
+    NEXT_POL_NUM += 1
+    QAP4.UpdateFrstLn("SecondHalf/Python/QAP4/Defaults.dat", (str(NEXT_POL_NUM)))
+
+    #Housekeeping
+    if not QAP4.Whoops("\nPress return to enter another customer, or type END and press return to exit the program: "):
+        print("Thank you for using the QAP4 claim processing program!")
+        print()
+        break
